a98dd971582649da3e7d3d9f275894602e14f619
ogbeche77 <ogbeche77@yahoo.com>
2020-01-30 20:26:03 +0100

    event added

--- Background Change/script.js
+++ Background Change/script.js
@@ -1,1 +1,11 @@
-function ()
+var change = document.querySelector (".btncolor");
+var changeBody = document.querySelector ("body");
+
+var colors = ["blue", "red", "green"]
+
+change.addEventListener("click", changeColor);
+
+function changeColor (){
+     let random = Math.floor(Math.random()* colors.length)
+     changeBody.style.backgroundColor = colors[random];
+}
