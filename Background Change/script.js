e9a966a6c31772c74653b881f85f6f152299477a
ogbeche77 <ogbeche77@yahoo.com>
2020-01-30 20:36:34 +0100

    more colors added

--- Background Change/script.js
+++ Background Change/script.js
@@ -1,7 +1,8 @@
 var change = document.querySelector (".btncolor");
 var changeBody = document.querySelector ("body");
 
-var colors = ["blue", "red", "green"]
+var colors = ["blue", "red", "green", "rgb(50, 168, 82)", "rgb(102, 122, 107)", "rgb(91, 207, 180)",
+"rgb(0, 59, 45)","rgb(45, 197, 224)", "rgb(250, 242, 5)" ]
 
 change.addEventListener("click", changeColor);
 
